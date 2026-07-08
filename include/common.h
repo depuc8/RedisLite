919ca62f4d890551c7b2766314a67a53b8777348
depuc8 <praveendeepak8koripalli@gmail.com>
2026-07-08 18:15:21 +0530

    fix(common): replace typeof with decltype for C++17 compatibility

--- include/common.h
+++ include/common.h
@@ -5,8 +5,8 @@
 
 
 // intrusive data structure
-#define container_of(ptr, type, member) ({                  \
-    const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+#define container_of(ptr, type, member) ({                      \
+    const decltype( ((type *)0)->member ) *__mptr = (ptr);      \
     (type *)( (char *)__mptr - offsetof(type, member) );})
 
 // FNV hash
